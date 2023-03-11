6a85b72b7ba54ed13f0646611c1042441d11c1ac
Stanislav Svetonosov <stas.svetonosov@gmail.com>
2023-03-11 20:25:01 +0300

    Feat:add home work 1

---
+++ seminar_1/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+// Console.WriteLine("Введите число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1>number2)
+// {
+//   Console.WriteLine($"число {number1} большее, число {number2} меньшее");
+// }
+//   else if(number1==number2)
+// {
+//   Console.WriteLine("вы ввели два равных числа");
+// }
+// else
+// {
+//   Console.WriteLine($"число {number2} большее, число {number1} меньшее");
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+// Console.WriteLine("Введите число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1<number2 && number2>number3)
+// {
+//   Console.WriteLine($"число {number2} максимальное");
+// }
+//   else if(number2<number1 && number1>number3)
+// {
+//   Console.WriteLine($"число {number1} максимальное");
+// }
+// else
+// {
+//   Console.WriteLine($"число {number3} максимальное");
+// }
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+// Console.WriteLine("Введите число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+ 
+//  if(number1 % 2 == 0 )
+//  {
+//   Console.Write("Введенное число является четным");
+//  }
+//   else
+//   {
+//     Console.Write("Введенное число нечетное");
+//   }
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+// Console.WriteLine("Введите число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// int index = 0;
+
+// while (index<number1)
+// {
+//   index += 2;
+//   if(index<number1)
+//   Console.Write(index);
+// }
---
+++ seminar_2/Program.cs
@@ -0,0 +1,97 @@
+﻿// int num = new Random().Next(3,11);
+
+// Console.WriteLine(num);
+
+
+// выводит случайное число из отрезка(10,100) и показывает наибольшее число
+
+// int num = new Random().Next(10,100);
+// Console.WriteLine(num);
+
+// int a = num/10;
+// int b = num%10;
+
+// if(a>b)
+// {
+//   Console.WriteLine("Наибольшая цифра: " +a);
+// }
+// else
+// {
+//   Console.WriteLine("наибольшая цифра: " +b);
+// }
+
+
+// Выводит случайное трехзначное число и удаляет второе число
+// Напишите программу, которая выводит случайное трехзначное 
+// число и удаляет вторую цифру этого числа.
+
+
+// int num =new Random().Next(100,1000);
+// Console.WriteLine(num);
+
+// int a = num/100;
+// int d = num%10;
+// int result = a*10+d;
+
+// Console.WriteLine(result);
+
+
+// Напишите программу, которая будет принимать на вход два 
+// числа и выводить, является ли второе число кратным первому.
+//  Если число 2 не кратно числу 1, то программа выводит 
+//  остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+// Console.WriteLine("Введите число 1");
+// int number1= Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число 2");
+// int number2= Convert.ToInt32(Console.ReadLine());
+
+// // int num =new Random().Next(100,1000);
+// // Console.WriteLine(num);
+
+// if(number1%number2==0)
+// {
+//     Console.WriteLine("Кратны");
+// }
+
+// else
+// {
+//     Console.WriteLine($"Не кратны, остаток от деления равен {number1%number2}");
+// }
+
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num%7==0&&num%23==0)
+// {
+//     Console.WriteLine("Кратны!");
+// }
+// else
+// {
+//     Console.WriteLine("Не кратны!");
+// }
+
+Console.WriteLine("Enter number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1==num2*num2)
+{
+    Console.WriteLine(num1 + " является квадратом " + num2);
+}
+else if  (num2==num1*num1)
+{
+    Console.WriteLine(num2 + " является квадратом " + num1);
+}
+else
+{
+    Console.WriteLine("Не являются квадратом");
+}
+
+
+
+
