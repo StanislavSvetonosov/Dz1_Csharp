a3417237809ad791b5cae6cea20c7680772ec252
Stanislav Svetonosov <stas.svetonosov@gmail.com>
2023-03-31 19:32:50 +0300

    feat:add homework_6

---
+++ home_work_6/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+// Console.Write("Введите числа ( через пробел ): ");
+// int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//   if(array[i]>0)
+//   {
+//     count++;
+//   }
+// }
+// Console.WriteLine($"Количество элементов больше 0 : {count} ");
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// Console.WriteLine("Введите значение b1 ");
+// var b1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите значение k1 ");
+// var k1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите значение b2 ");
+// var b2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите значение k2 ");
+// var k2 = Convert.ToDouble(Console.ReadLine());
+// var x = (b2-b1)/(k1-k2);
+// var y = k1*x + b1;
+ 
+//  if(b1==b2)
+//  {
+//     Console.WriteLine($" Прямые совпадают ");
+//  }
+
+//  else if (k1==k2)
+// {
+//   Console.WriteLine($" Прямые параллельны");
+// }
+// else
+// {
+//   Console.WriteLine($" Пересечение в точке ( { x } ,{ y } )");
+// }
